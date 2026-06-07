cc2ff7dac05a43b1d9e1c0e5206666f7a5094845
SATISHTEJA <vemulasatishteja537@gmail.com>
2026-06-07 10:50:02 +0530

    Fixed Interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,14 @@
 import { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-    const [features, setFeatures] = useState(
-        Array(24).fill("")
-    );
-
+    const [features, setFeatures] = useState(Array(24).fill(""));
     const [result, setResult] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-    const handleChange = (index, value) => {
-        const updated = [...features];
-        updated[index] = value;
-        setFeatures(updated);
-    };
-
-    const predictRisk = async () => {
-        try {
-            if (features.some((item) => item === "")) {
-                alert("Please fill all fields");
-                return;
-            }
-            const response = await axios.post(
-                "https://credit-delinquency-prediction.onrender.com/predict",
-                {
-                    features: features.map(Number),
-                }
-            );
-
-            setResult(response.data);
-        } catch (error) {
-            console.error(error);
-            alert("Prediction Failed");
-        }
-    };
     const labels = [
         "Checking Account Status",
         "Duration",
@@ -58,72 +33,194 @@ function App() {
         "Feature 21",
         "Feature 22",
         "Feature 23",
-        "Feature 24"
+        "Feature 24",
     ];
 
+    const handleChange = (index, value) => {
+        const updated = [...features];
+        updated[index] = value;
+        setFeatures(updated);
+    };
+
+    const predictRisk = async () => {
+        try {
+            if (features.some((item) => item === "")) {
+                toast.warning("Please fill all fields");
+                return;
+            }
+
+            if (features.some((item) => Number(item) < 0)) {
+                toast.error("Negative values are not allowed");
+                return;
+            }
+
+            setLoading(true);
+
+            const response = await axios.post(
+                "https://credit-delinquency-prediction.onrender.com/predict",
+                {
+                    features: features.map(Number),
+                }
+            );
+
+            setResult(response.data);
+            toast.success("Prediction completed successfully!");
+        } catch (error) {
+            console.error(error);
+            toast.error("Prediction Failed");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const riskPercentage = result
+        ? (result.risk_probability * 100).toFixed(0)
+        : 0;
+
     return (
-        <div className="container">
-            <h1>AI-Powered Credit Delinquency Predictor</h1>
+        <div className="app">
+            <div className="dashboard">
 
-            <div className="grid">
-                {features.map((value, index) => (
-                    <input
-                        key={index}
-                        type="number"
-                        placeholder={labels[index]}
-                        value={value}
-                        onChange={(e) =>
-                            handleChange(index, e.target.value)
-                        }
-                    />
-                ))}
-            </div>
+                <div className="header">
+                    <h1>💳 Credit Delinquency Predictor</h1>
+                    <p>
+                        AI-Powered Credit Risk Assessment Dashboard
+                    </p>
+                </div>
 
-            <button onClick={predictRisk}>
-                Predict Risk
-            </button>
+                <div className="top-cards">
+                    <div className="stat-card">
+                        <h3>Accuracy</h3>
+                        <span>89%</span>
+                    </div>
 
-            {result && (
-                <div className="result">
-                    <h2
-                        style={{
-                            color:
-                                result.prediction === 1
-                                    ? "red"
-                                    : "green",
-                        }}
-                    >
-                        {result.prediction === 1
-                            ? "🔴 High Risk"
-                            : "🟢 Low Risk"}
-                    </h2>
+                    <div className="stat-card">
+                        <h3>Features</h3>
+                        <span>24 Inputs</span>
+                    </div>
 
-                    <h3>
-                        Risk Score: {(result.risk_probability * 100).toFixed(2)}%
-                    </h3>
-                    <div className="progress">
-                        <div
-                            className="progress-fill"
-                            style={{
-                                width: `${result.risk_probability * 100}%`,
-                                background:
-                                    result.risk_probability > 0.7
-                                        ? "#ef4444"
-                                        : result.risk_probability > 0.4
-                                            ? "#f59e0b"
-                                            : "#22c55e",
-                            }}
-                        />
-                        <p>
-                            {result.risk_probability > 0.7
-                                ? "High Delinquency Risk"
-                                : result.risk_probability > 0.4
-                                    ? "Medium Delinquency Risk"
-                                    : "Low Delinquency Risk"}
-                        </p>
+                    <div className="stat-card">
+                        <h3>Prediction</h3>
+                        <span>Real-Time</span>
                     </div>
                 </div>
-            )}
+
+                <div className="main-section">
+
+                    <div className="form-panel">
+                        <h2>Customer Information</h2>
+
+                        <div className="grid">
+                            {features.map((value, index) => (
+                                <div className="input-group" key={index}>
+                                    <label>{labels[index]}</label>
+
+                                    <input
+                                        key={index}
+                                        type="number"
+                                        min="0"
+                                        placeholder={labels[index]}
+                                        value={value}
+                                        onChange={(e) => {
+                                            const value = e.target.value;
+
+                                            if (value === "" || Number(value) >= 0) {
+                                                handleChange(index, value);
+                                            }
+                                        }}
+                                        onKeyDown={(e) => {
+                                            if (e.key === "-" || e.key === "e") {
+                                                e.preventDefault();
+                                            }
+                                        }}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+
+                        <button
+                            onClick={predictRisk}
+                            className="predict-btn"
+                            disabled={loading}
+                        >
+                            {loading
+                                ? "Analyzing..."
+                                : "Predict Credit Risk"}
+                        </button>
+                    </div>
+
+                    <div className="result-panel">
+                        <h2>Risk Analysis</h2>
+
+                        {result ? (
+                            <>
+                                <div className="gauge-wrapper">
+                                    <div
+                                        className="gauge"
+                                        style={{
+                                            background: `conic-gradient(
+                        ${riskPercentage > 70
+                                                    ? "#ef4444"
+                                                    : riskPercentage > 40
+                                                        ? "#f59e0b"
+                                                        : "#22c55e"
+                                                } ${riskPercentage * 3.6}deg,
+                        #334155 0deg
+                      )`,
+                                        }}
+                                    >
+                                        <div className="gauge-inner">
+                                            <h1>{riskPercentage}%</h1>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div
+                                    className={`risk-badge ${result.prediction === 1
+                                        ? "high"
+                                        : "low"
+                                        }`}
+                                >
+                                    {result.prediction === 1
+                                        ? "🔴 High Risk"
+                                        : "🟢 Low Risk"}
+                                </div>
+
+                                <div className="analysis-card">
+                                    <h3>Risk Score</h3>
+                                    <p>{riskPercentage}%</p>
+                                </div>
+
+                                <div className="analysis-card">
+                                    <h3>AI Recommendation</h3>
+
+                                    <p>
+                                        {result.risk_probability > 0.7
+                                            ? "Applicant has a high probability of delinquency. Additional verification or collateral is recommended."
+                                            : result.risk_probability > 0.4
+                                                ? "Moderate risk profile. Further review is advised."
+                                                : "Low risk applicant with strong repayment potential."}
+                                    </p>
+                                </div>
+                            </>
+                        ) : (
+                            <div className="empty-state">
+                                <h3>📊 Awaiting Prediction</h3>
+                                <p>
+                                    Fill customer details and click
+                                    Predict Risk.
+                                </p>
+                            </div>
+                        )}
+                    </div>
+
+                </div>
+            </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                theme="dark"
+            />
         </div>
     );
 }
